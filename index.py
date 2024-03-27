f2e5d89687dea5d36126f6b55f9558aaf3d5b2b6
aranzate <rafael.aranzate@gmail.com>
2024-03-27 00:52:29 -0300

    Adicionada funcionalidade de busca e precision/recall

--- index.py
+++ index.py
@@ -17,14 +17,23 @@ class Index:
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         with open(f".\\logs\\{self.index_documents.__name__}_{timestamp}.txt", 'w', encoding='utf-8') as file:
             file.write(f"Função {self.index_documents.__name__} no arquivo {file_path}\n")
-        
-        for body in data[array_name]:
-            calculate_action_execution_time(self.es.insert_document, self.index_documents.__name__, timestamp, self.index, body)
-        
+
+        for doc_id, body in enumerate(data[array_name], start=1):
+            # Aqui, doc_id é usado como o ID do documento. Você pode ajustar isso conforme necessário.
+            calculate_action_execution_time(self.es.insert_document, self.index_documents.__name__, timestamp, self.index, body, doc_id)
+            
     # Adiciona os documentos de uma vez, ao elastic_search
     def index_documents_bulk(self, file_path, array_name):
         with open(file_path, 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)[array_name]
             
-        calculate_function_execution_time(self.es.insert_documents, self.index, data) 
+        # Modificar data para incluir IDs
+        indexed_data = []
+        for idx, item in enumerate(data):
+            # Adicionar um ID para cada documento (pode ser qualquer lógica que você preferir)
+            item['_id'] = idx + 1  # Aqui estou usando os índices baseados em 1 como ID
+            
+            indexed_data.append(item)
+            
+        calculate_function_execution_time(self.es.insert_documents, self.index, indexed_data)
         
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from search import Search
 from index import Index
 import urllib3
+import json
 
 # desabilita avisos
 urllib3.disable_warnings() 
 
 # Inicializa variáveis globais
 INDEX = "contextos"
-FILE_PATH = ".\\apagar.json" #".\\squad-v1.1-pt-master\\contexts.json" 
+FILE_PATH = ".\\squad-v1.1-pt-master\\contexts.json"
+QUERIES_PATH = ".\\squad-v1.1-pt-master\\questions.json"
 ARRAY_NAME = 'contexts'
+SEARCHED_DOCUMENTS_QUANTITY = 10
 
 # Cria objeto de busca no elastic search
 es = Search()
@@ -16,6 +19,30 @@ es = Search()
 # Cria o objeto da classe de indexação
 index = Index(es, INDEX)
 
-es.create_index(INDEX)
-index.index_documents(FILE_PATH, ARRAY_NAME)
-# index.index_documents_bulk(FILE_PATH, ARRAY_NAME)
+# es.create_index(INDEX)
+# index.index_documents(FILE_PATH, ARRAY_NAME)
+# #index.index_documents_bulk(FILE_PATH, ARRAY_NAME)
+# queries = es.find_queries(QUERIES_PATH)
+
+
+# #Escreva as respostas encontrados no JSON
+# answers = es.find_answers(QUERIES_PATH)
+# output_answers_path = "answers.json"
+# with open(output_answers_path, 'w') as json_file:
+#     json.dump(answers, json_file, indent=4)
+
+# # Escreva os documentos encontrados no JSON
+# found_documents = es.linear_search(queries, SEARCHED_DOCUMENTS_QUANTITY)
+# output_file_path = "found.json"
+# with open(output_file_path, 'w') as json_file:
+#      json.dump(found_documents, json_file, indent=4)
+
+
+with open('found.json', 'r') as found_file:
+    found_documents = json.load(found_file)
+with open('answers.json', 'r') as answers_file:
+    answers = json.load(answers_file)
+
+es.precision_at_k(found_documents, answers, 10)
+es.recall_at_k(found_documents, answers, 10)
+
