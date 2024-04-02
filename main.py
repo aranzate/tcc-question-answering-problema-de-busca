e3011bdc306757f9a1786e3765ec71e5b4b6a2a3
btrcardoso <btrcardos@gmail.com>
2024-04-01 23:42:51 -0300

    refactor: cria arquivos para determinadas funções

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from search import Search
 from index import Index
+from utils import *
+from metrics import *
 import urllib3
 import json
 
@@ -8,8 +10,8 @@ urllib3.disable_warnings()
 
 # Inicializa variáveis globais
 INDEX = "contextos"
-FILE_PATH = ".\\squad-v1.1-pt-master\\contexts.json"
-QUERIES_PATH = ".\\squad-v1.1-pt-master\\questions.json"
+FILE_PATH = ".\\ignorar\\contexts.json" # ".\\squad-v1.1-pt-master\\contexts.json"
+QUERIES_PATH = ".\\ignorar\\questions.json" # ".\\squad-v1.1-pt-master\\questions.json"
 ARRAY_NAME = 'contexts'
 SEARCHED_DOCUMENTS_QUANTITY = 10
 
@@ -22,27 +24,27 @@ index = Index(es, INDEX)
 es.create_index(INDEX)
 #index.index_documents(FILE_PATH, ARRAY_NAME)
 index.index_documents_bulk(FILE_PATH, ARRAY_NAME)
-queries = es.find_queries(QUERIES_PATH)
+queries = find_queries(QUERIES_PATH)
 
 
 #Escreva as respostas encontrados no JSON
-answers = es.find_answers(QUERIES_PATH)
-output_answers_path = "answers.json"
+answers = find_answers(QUERIES_PATH)
+output_answers_path = ".\\results\\answers.json"
 with open(output_answers_path, 'w') as json_file:
     json.dump(answers, json_file, indent=4)
 
 # Escreva os documentos encontrados no JSON
-found_documents = es.linear_search(queries, SEARCHED_DOCUMENTS_QUANTITY)
-output_file_path = "found.json"
+found_documents = linear_search(es, queries, SEARCHED_DOCUMENTS_QUANTITY)
+output_file_path = ".\\results\\found.json"
 with open(output_file_path, 'w') as json_file:
      json.dump(found_documents, json_file, indent=4)
 
 # Carrega dados obtidos dos JSON para calcular precision@k e recall@k
-with open('found.json', 'r') as found_file:
+with open('.\\results\\found.json', 'r') as found_file:
     found_documents = json.load(found_file)
-with open('answers.json', 'r') as answers_file:
+with open('.\\results\\answers.json', 'r') as answers_file:
     answers = json.load(answers_file)
 
-es.precision_at_k(found_documents, answers, 10)
-es.recall_at_k(found_documents, answers, 10)
+precision_at_k(found_documents, answers, 10)
+recall_at_k(found_documents, answers, 10)
 
---
+++ utils.py
@@ -0,0 +1,53 @@
+import json
+import json
+from datetime import datetime
+from benchmark_recorder import calculate_action_execution_time, calculate_function_execution_time
+
+def linear_search(es, queries, quantity):
+    found_documents = {query.get('id_question'): [] for query in queries}
+    
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    for query in queries:
+        id = query['id_question']
+        question = query['question']
+
+        query_busca = {
+            "size": quantity,
+            "query": {
+                "match": {
+                    "context": question  # Busca apenas no contexto específico
+                }
+            }
+        }
+        
+        results = calculate_action_execution_time(es.search, linear_search.__name__, timestamp, index="contextos", body=query_busca)
+
+        found_documents[id].extend(hit['_id'] for hit in results['hits']['hits'])
+    
+    return found_documents
+    
+# retorna o json de queries 
+def find_queries(queries_file):
+    with open(queries_file, 'r') as file:
+        data = json.load(file)
+        queries = data['questions']
+    return queries
+
+# retorna o json com a relação  <{ id_da_pergunta: [id_do_contexto_com_a_resposta] } >
+def find_answers(queries_file):
+    mapping = {}
+
+    with open(queries_file, 'r') as file:
+        data = json.load(file)
+        questions = data['questions']
+
+        for question in questions:
+            question_id = str(question['id_question'])
+            context_id = str(question['id_context'])
+
+            if question_id not in mapping:
+                mapping[question_id] = []
+
+            mapping[question_id].append(context_id)
+
+    return mapping
