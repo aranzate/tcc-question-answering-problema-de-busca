a0b81a5fc21d116b239b7c869977bcad261f2814
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-05-26 11:36:52 -0300

    style: muda posição das variaveis em consts.py

--- consts.py
+++ consts.py
@@ -7,8 +7,10 @@ ARRAY_NAME = 'contexts'
 SEARCHED_DOCUMENTS_QUANTITY = 10
 
 if(system == "linux"):
-    FILE_PATH = "./ignorar/contexts.json" # "./squad-v1.1-pt-master/contexts.json"
-    QUERIES_PATH = "./ignorar/questions.json" # "./squad-v1.1-pt-master/questions.json"
+    # FILE_PATH = "./ignorar/contexts.json"
+    # QUERIES_PATH = "./ignorar/questions.json" 
+    FILE_PATH = "./squad-v1.1-pt-master/contexts.json"
+    QUERIES_PATH = "./squad-v1.1-pt-master/questions.json"
     RESULT_ANSWERS_PATH = "./results/answers.json"
     RESULT_FOUND_PATH = "./results/found.json"
     LOGS_PATH = "./logs/"
