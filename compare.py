67bf17151e247930bcc2a2efbd5c2db4a42c5bfb
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-06-02 16:49:14 -0300

    feat: ajusta escala dos graficos

--- compare.py
+++ compare.py
@@ -32,6 +32,11 @@ df = linear_search.merge(linear_msearch, on='id').merge(parallel_search, on='id'
 fig, axs = plt.subplots(4, 1, figsize=(10, 20), gridspec_kw={'hspace': 0.7})
 plt.subplots_adjust(left=0.048, bottom=0.067, right= 0.985, top=0.96)
 fig.text(0.05, 0.95, f'Nodes: {nodes}, Shards: {shards}\n')
+
+# Definir os limites dos eixos x e y
+x_limits = (df['id'].min(), df['id'].max())
+y_limits = (0,.11) # (min(df['tempo_linear_search'].min(), df['tempo_linear_msearch'].min()), max(df['tempo_linear_search'].max(), df['tempo_linear_msearch'].max()))
+
 # Subplot 1: Linear Search
 axs[0].plot(df['id'], df['tempo_linear_search'], label='Linear Search', color='blue')
 axs[0].set_xlabel('ID', labelpad=-9)
@@ -39,6 +44,8 @@ axs[0].set_ylabel('Tempo (s)')
 axs[0].set_title('Linear Search')
 axs[0].grid(True)
 axs[0].text(0.88, 0.95, f'Tempo: {time_linear:.2f} s', transform=axs[0].transAxes, fontsize=10, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
+axs[0].set_xlim(x_limits)
+axs[0].set_ylim(y_limits)
 
 # Subplot 2: Linear MSearch
 axs[1].plot(df['id'], df['tempo_linear_msearch'], label='Linear MSearch', color='red')
@@ -47,6 +54,8 @@ axs[1].set_ylabel('Tempo (s)')
 axs[1].set_title('Linear MSearch')
 axs[1].grid(True)
 axs[1].text(0.88, 0.95, f'Tempo: {time_msearch:.2f} s', transform=axs[1].transAxes, fontsize=10, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
+axs[1].set_xlim(x_limits)
+axs[1].set_ylim(y_limits)
 
 # Subplot 3: Parallel Search
 axs[2].plot(df['id'], df['tempo_parallel_search'], label='Parallel Search', color='green')
@@ -55,6 +64,8 @@ axs[2].set_ylabel('Tempo (s)')
 axs[2].set_title('Parallel Search')
 axs[2].grid(True)
 axs[2].text(0.88, 0.95, f'Tempo: {time_parallel:.2f} s', transform=axs[2].transAxes, fontsize=10, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
+axs[2].set_xlim(x_limits)
+axs[2].set_ylim(y_limits)
 
 # Subplot 4: Combined
 axs[3].plot(df['id'], df['tempo_linear_search'], label='Linear Search', color='blue')
@@ -64,6 +75,8 @@ axs[3].set_xlabel('ID', labelpad=-9)
 axs[3].set_ylabel('Tempo (s)')
 axs[3].set_title('Comparação dos Tempos de Resposta das Consultas')
 axs[3].grid(True)
+axs[3].set_xlim(x_limits)
+axs[3].set_ylim(y_limits)
 
 # Ajustar a legenda para fora do gráfico
 axs[3].legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=3)
