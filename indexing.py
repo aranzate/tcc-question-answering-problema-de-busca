45fcd268797f0874a4147853dbe7e767b24cba7b
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-06-08 17:11:17 -0300

    feat: separa indexing e searching

---
+++ indexing.py
@@ -0,0 +1,15 @@
+from search import Search
+from index import Index
+from utils import *
+from metrics import *
+import urllib3
+import consts
+urllib3.disable_warnings() 
+
+es = Search()
+index = Index(es, consts.INDEX)
+es.create_index(consts.INDEX, consts.SHARDS)
+# index.index_documents(consts.FILE_PATH, consts.ARRAY_NAME, shards, nodes)
+index.index_documents_bulk(consts.FILE_PATH, consts.ARRAY_NAME, consts.SHARDS, consts.NODES)
+
+
