9f70a91206d47f56e37d98fd9d77ed66ddfa3670
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-06-08 20:31:17 -0300

    feat: passagem de parametros para executar searching

--- searching.py
+++ searching.py
@@ -4,32 +4,49 @@ from metrics import *
 import urllib3
 import json
 import consts
+import argparse
 urllib3.disable_warnings() 
 
-es = Search()
-nodes = es.nodes_quantity()
-shards = es.shards_quantity(consts.INDEX)
+def main():
+    parser = argparse.ArgumentParser(description='Executa uma função específica com base no parâmetro passado.')
+    options = "ls - linear_search, lm - linear_msearch, ps - parallel_search."
+    parser.add_argument('func', type=str, help='Alias da função a ser executada: ' + options)
+    args = parser.parse_args()
 
-#Escreva as respostas encontrados no JSON
-queries = find_queries(consts.QUERIES_PATH)
-answers = find_answers(consts.QUERIES_PATH)
-output_answers_path = consts.RESULT_ANSWERS_PATH
-with open(output_answers_path, 'w') as json_file:
-    json.dump(answers, json_file, indent=4)
+    # Inicializa objeto do elastic search
+    es = Search()
+    nodes = es.nodes_quantity()
+    shards = es.shards_quantity(consts.INDEX)
 
-# Escreva os documentos encontrados no JSON
-found_documents = linear_search(es, queries, consts.SEARCHED_DOCUMENTS_QUANTITY, shards, nodes)
-# found_documents = linear_msearch(es, queries, consts.SEARCHED_DOCUMENTS_QUANTITY, shards, nodes)
-# found_documents = parallel_search(es, queries, consts.SEARCHED_DOCUMENTS_QUANTITY, shards, nodes)
-output_file_path = consts.RESULT_FOUND_PATH
-with open(output_file_path, 'w') as json_file:
-    json.dump(found_documents, json_file, indent=4)
+    #Escreva as respostas encontrados no JSON
+    queries = find_queries(consts.QUERIES_PATH)
+    answers = find_answers(consts.QUERIES_PATH)
+    output_answers_path = consts.RESULT_ANSWERS_PATH
+    with open(output_answers_path, 'w') as json_file:
+        json.dump(answers, json_file, indent=4)
 
-# Carrega dados obtidos dos JSON para calcular precision@k e recall@k
-with open(consts.RESULT_FOUND_PATH, 'r') as found_file:
-    found_documents = json.load(found_file)
-with open(consts.RESULT_ANSWERS_PATH, 'r') as answers_file:
-    answers = json.load(answers_file)
+    # Realiza a busca de acordo com os parâmetros passados
+    if args.func == 'ls':
+        found_documents = linear_search(es, queries, consts.SEARCHED_DOCUMENTS_QUANTITY, shards, nodes)
+    elif args.func == 'lm':
+        found_documents = linear_msearch(es, queries, consts.SEARCHED_DOCUMENTS_QUANTITY, shards, nodes)
+    elif args.func == 'ps':
+        found_documents = parallel_search(es, queries, consts.SEARCHED_DOCUMENTS_QUANTITY, shards, nodes)
+    else:
+        print("Função não reconhecida. As opções são: " + options)
 
-precision_at_k(found_documents, answers, 10)
-recall_at_k(found_documents, answers, 10)
+    # Escreva os documentos encontrados no JSON
+    output_file_path = consts.RESULT_FOUND_PATH
+    with open(output_file_path, 'w') as json_file:
+        json.dump(found_documents, json_file, indent=4)
+
+    # Carrega dados obtidos dos JSON para calcular precision@k e recall@k
+    with open(consts.RESULT_FOUND_PATH, 'r') as found_file:
+        found_documents = json.load(found_file)
+    with open(consts.RESULT_ANSWERS_PATH, 'r') as answers_file:
+        answers = json.load(answers_file)
+    precision_at_k(found_documents, answers, 10)
+    recall_at_k(found_documents, answers, 10)
+
+if __name__ == '__main__':
+    main()
