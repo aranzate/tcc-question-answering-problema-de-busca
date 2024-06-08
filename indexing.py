7e828c43f269b4b1435106bddac172af8b191f9c
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-06-08 20:37:47 -0300

    feat: passa argumentos para indexing

--- indexing.py
+++ indexing.py
@@ -4,19 +4,29 @@ from utils import *
 from metrics import *
 import urllib3
 import consts
+import argparse
 urllib3.disable_warnings() 
 
-es = Search()
-index = Index(es, consts.INDEX)
-nodes = es.nodes_quantity()
-shards = es.shards_quantity(consts.INDEX)
+def main():
+    parser = argparse.ArgumentParser(description='Executa uma função específica com base no parâmetro passado.')
+    options = "id - index_documents, ib - index_documents_bulk"
+    parser.add_argument('func', type=str, help='Alias da função a ser executada: ' + options)
+    args = parser.parse_args()
 
-es.create_index(consts.INDEX, shards)
+    es = Search()
+    index = Index(es, consts.INDEX)
+    nodes = es.nodes_quantity()
+    shards = es.shards_quantity(consts.INDEX)
 
-# indexação linear
-# index.index_documents(consts.FILE_PATH, consts.ARRAY_NAME, shards, nodes)
+    es.create_index(consts.INDEX, shards)
 
-# indexação bulk
-index.index_documents_bulk(consts.FILE_PATH, consts.ARRAY_NAME, shards, nodes)
+    if args.func == 'id':
+        index.index_documents(consts.FILE_PATH, consts.ARRAY_NAME, shards, nodes)
+    elif args.func == 'ib':
+        index.index_documents_bulk(consts.FILE_PATH, consts.ARRAY_NAME, shards, nodes)
+    else:
+        print("Função não reconhecida. As opções são: " + options)
 
+if __name__ == '__main__':
+    main()
 
