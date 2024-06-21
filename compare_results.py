ddd0ce72a285e264546a4f1be62a24efce6ed9bd
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-06-21 12:53:34 -0300

    refactor: muda nome de funções e remove comentarios

--- compare_results.py
+++ compare_results.py
@@ -7,19 +7,7 @@ def read_json(filepath):
     with open(filepath, 'r') as file:
         data = json.load(file)
 
-    time_python_function=data['time_python_function']
-    mean=data['mean']
-    variance=data['variance']
-    standard_deviation=data['standard_deviation']
-    
-    return time_python_function, mean, variance, standard_deviation
-
-def read_json2(filepath):
-    with open(filepath, 'r') as file:
-        data = json.load(file)
-
     return data
-
 
 def params():
     result = [
@@ -80,15 +68,15 @@ def get_data(nodes_list, shards_list, folder_name):
 
         for shards in shards_list:
 
-            json_data = read_json2(f"{folder_name}n{nodes}_s{shards}_log_linear_search.json")
+            json_data = read_json(f"{folder_name}n{nodes}_s{shards}_log_linear_search.json")
             for attribute in attributes:
                 attribute["ls"].append(json_data[attribute["param"]])
 
-            json_data = read_json2(f"{folder_name}n{nodes}_s{shards}_log_linear_msearch.json") 
+            json_data = read_json(f"{folder_name}n{nodes}_s{shards}_log_linear_msearch.json") 
             for attribute in attributes:
                 attribute["lm"].append(json_data[attribute["param"]])
 
-            json_data = read_json2(f"{folder_name}n{nodes}_s{shards}_log_parallel_search.json")
+            json_data = read_json(f"{folder_name}n{nodes}_s{shards}_log_parallel_search.json")
             for attribute in attributes:
                 attribute["ps"].append(json_data[attribute["param"]])
 
@@ -104,8 +92,6 @@ def main():
     parser = argparse.ArgumentParser(description='Executa uma função específica com base no parâmetro passado.')
     parser.add_argument('folder_name', nargs="?", default="", help='Nome da pasta')
     args = parser.parse_args()
-    
-    print("GRAFICOS: Gera gráficos de comparação com todos os logs")
 
     folder_name = args.folder_name
     if(folder_name == "") or (folder_name is None): 
@@ -113,26 +99,22 @@ def main():
 
     nodes_list = [1,2,3,4]
     shards_list = [1,2,4,8,12,16,32]
-
     attributes = get_data(nodes_list, shards_list, folder_name)
+
+    print("GRAFICOS: Gera gráficos de comparação com todos os logs")
     
     for attribute in attributes:
+        columns_title = ['L. Search', 'L. Msearch', 'Parallel Search']
+        rows_title = ['n' + str(nodes) for nodes in nodes_list] # ['n1', 'n2', 'n3', 'n4']
+        x = shards_list
+        y = attribute["total"] 
         min = attribute["min"]
         max = attribute["max"]
-        y = attribute["total"] 
         title = attribute["title"] 
         
-        columns_title = ['L. Search', 'L. Msearch', 'Parallel Search']
-        x = shards_list
-
-        # Configurações da grade de gráficos
         fig, axes = plt.subplots(len(nodes_list), len(columns_title), figsize=(15, 20))
-        fig.suptitle(title, fontsize=16)
+        fig.suptitle(title, fontsize=16)        
 
-        # Títulos das linhas e colunas
-        linhas_titulo = ['n' + str(nodes) for nodes in nodes_list] # ['n1', 'n2', 'n3', 'n4']
-
-        # Gerar gráficos
         for i in range(len(nodes_list)):
             for j in range(len(columns_title)):
                 ax = axes[i, j]
@@ -148,15 +130,13 @@ def main():
         # Adicionar títulos às linhas e colunas
         for ax, col in zip(axes[0], columns_title):
             ax.set_title(col, fontsize=14, pad=20)
-
-        for ax, row in zip(axes[:,0], linhas_titulo):
+        for ax, row in zip(axes[:,0], rows_title):
             ax.set_ylabel(row, rotation=0, fontsize=14, labelpad=40)
 
-        # Caminho completo do arquivo
+        # Salvar
         fig_path = f'{folder_name}/results_{title.replace(" ", "_").lower()}.png'
         plt.savefig(fig_path)
         plt.close(fig)
-
 
 
 if __name__ == '__main__':
