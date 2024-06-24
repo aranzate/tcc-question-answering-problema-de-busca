c855859d0ca396a12d5990d06bf1afebdd5fccf0
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-06-24 19:14:44 -0300

    refactor: adiciona contextos por variavel de ambiente

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ def linear_search(es, queries, quantity, shards, nodes, folder_name):
         found_documents[id].extend(hit['_id'] for hit in results['hits']['hits'])
         
     end_time = time.time()
-    write_log(linear_search.__name__, "contextos do elastic search", es.search.__name__, actions, timestamp, shards=shards, nodes=nodes, time_python_function=end_time-start_time, folder_name=folder_name)
+    write_log(linear_search.__name__, consts.INDEX, es.search.__name__, actions, timestamp, shards=shards, nodes=nodes, time_python_function=end_time-start_time, folder_name=folder_name)
     return found_documents
 
 def linear_msearch(es, queries, quantity, shards, nodes, folder_name):
@@ -73,7 +73,7 @@ def linear_msearch(es, queries, quantity, shards, nodes, folder_name):
             found_documents[id].extend(hit['_id'] for hit in result['hits']['hits'])
     end_time = time.time()
     
-    write_log(linear_msearch.__name__, "contextos do elastic search", "msearch", actions, timestamp, shards=shards, nodes=nodes, time_python_function=end_time-start_time, folder_name=folder_name)
+    write_log(linear_msearch.__name__, consts.INDEX, "msearch", actions, timestamp, shards=shards, nodes=nodes, time_python_function=end_time-start_time, folder_name=folder_name)
     return found_documents
 
 def parallel_search(es, queries, quantity, shards, nodes, folder_name):
@@ -112,7 +112,7 @@ def parallel_search(es, queries, quantity, shards, nodes, folder_name):
         actions.append(action_time)
         found_documents[id].extend(hit['_id'] for hit in result['hits']['hits'])
 
-    write_log(parallel_search.__name__, "contextos do elastic search", "msearch", actions, timestamp, shards=shards, nodes=nodes, time_python_function=end_time-start_time, folder_name=folder_name)
+    write_log(parallel_search.__name__, consts.INDEX, "msearch", actions, timestamp, shards=shards, nodes=nodes, time_python_function=end_time-start_time, folder_name=folder_name)
     return found_documents
     
 # retorna o json de queries 
