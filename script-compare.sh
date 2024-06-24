b0ff44dd13d777c1750154461413b52cb8b025fd
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-06-24 11:04:06 -0300

    feat: atualiza variaveis de ambiente

--- script-compare.sh
+++ script-compare.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 
-# Realiza comparações e armazena tudo na pasta especificada em logs. 
+# Realiza comparações e armazena tudo na pasta especificada. 
 
 source .env.local
 
-nodes_list=(1 2 3 4)
-shards_list=(1 2 4 8 12 16 32)
-
-# Nome da pasta com timestamp 
-# timestamp=$(date +"%Y-%m-%d_%H-%M-%S") 
-# folder_name=".${SEPARATOR_PATH}${LOGS_PATH}_${timestamp}"
-# mkdir "$folder_name"
-# folder_name="${folder_name}/"
-
-# Nome da pasta sem timestamp
-# folder_name=".${SEPARATOR_PATH}${LOGS_PATH}"
-# mkdir "$folder_name"
-# folder_name="${folder_name}/"
-
-# Sem pasta
-# folder_name=""
-
-# Nome da pasta personalizado
-folder_name=".${SEPARATOR_PATH}${LOGS_PATH}contextos-0621"
+folder_name=".${SEPARATOR_PATH}${LOGS_PATH}${FOLDER_PATH}"
 mkdir "$folder_name"
 folder_name="${folder_name}/"
 
-for nodes in "${nodes_list[@]}"; do
-    for shards in "${shards_list[@]}"; do
+for nodes in "${NODES_LIST[@]}"; do
+    for shards in "${SHARDS_LIST[@]}"; do
         python3 compare.py $nodes $shards $folder_name
     done
 done
