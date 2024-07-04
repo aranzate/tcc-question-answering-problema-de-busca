6544ef83a014cc8d6bcffab08b1e1604ef84ec4d
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2024-07-03 23:36:08 -0300

    feat: script de comparacoes gerais

--- script-compare.sh
+++ script-compare.sh
@@ -6,10 +6,12 @@ source .env.local
 
 folder_name=".${SEPARATOR_PATH}${LOGS_PATH}${FOLDER_PATH}${SEPARATOR_PATH}"
 
-for nodes in "${NODES_LIST[@]}"; do
-    for shards in "${SHARDS_LIST[@]}"; do
-        python3 compare.py $nodes $shards $folder_name
-    done
-done
+# gráficos de comparação um por um
+# for nodes in "${NODES_LIST[@]}"; do
+#     for shards in "${SHARDS_LIST[@]}"; do
+#         python3 compare.py $nodes $shards $folder_name
+#     done
+# done
 
+# gráficos de comparação gerais
 python compare_results.py $folder_name --nodes_list "${NODES_LIST[@]}" --shards_list "${SHARDS_LIST[@]}"
